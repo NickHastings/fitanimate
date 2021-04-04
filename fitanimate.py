8173cbf5c99133549c11e88092e24115132c5a0e
Nick Hastings <nicholaschastings@gmail.com>
2021-04-04 15:05:02 +0900

    Choice between viertical and horizonal bar charts.

--- fitanimate.py
+++ fitanimate.py
@@ -6,14 +6,15 @@ import glob
 import fitparse
 from datetime import datetime
 
-import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
+plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
 class TextLine:
-    def __init__(self, axes, field_name, txt_format, x, y ):
+    def __init__(self, axes, field_name, txt_format, x=0.0, y=0.0 ):
         self.axes = axes
         self.field_name = field_name
         self.txt_format = txt_format
@@ -37,11 +38,12 @@ class TextLine:
         if not (self.field_name in data):
             return False
 
+
         self.value = data[self.field_name]
         return True
 
 class CounterTextLine(TextLine):
-    def __init__(self,axes, field_name, txt_format, x, y ):
+    def __init__(self,axes, field_name, txt_format, x=0.0, y=0.0 ):
         TextLine.__init__(self, axes, field_name, txt_format, x, y )
 
     def setValue(self, data):
@@ -51,55 +53,159 @@ class CounterTextLine(TextLine):
 
         return False
 
-class TextPlot:
-    def __init__(self, axes):
-        self.axes = axes
+class TSTextLine(TextLine):
+    def setValue(self,data):
+        if not TextLine.setValue(self, data):
+            return False
 
-        self.txt_lines = []
-        self.txt_lines.append( CounterTextLine( self.axes, 'lap', 'Lap {}',   -0.5, 1.0 ) )
-        self.txt_lines.append( TextLine( self.axes,'temperature', '{:.0f} ℃',    -0.5, 0.7 ) )
-        self.txt_lines.append( TextLine( self.axes,'altitude',    '{:.0f} m', -0.5, 0.4 ) )
+        self.value = datetime.fromtimestamp(int(self.value))
+        return True
+
+
+
+class TextPlot:
+    def __init__(self, axes, x=-0.11, y=0.92, dx=0.0, dy=-0.08):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.axes = axes
+        self.axes.set_axis_off()
+
+        self.textLines = []
+        self._addTextLine( TSTextLine( self.axes,'timestamp', '{}' ))
+        self._addTextLine( TextLine( self.axes,'temperature', '{:.0f} ℃'))
+        self._addTextLine( TextLine( self.axes,'altitude',    '{:.0f} m'))
+        self._addTextLine( TextLine( self.axes,'heart_rate',  '{:.0f} BPM'))
+        self._addTextLine( CounterTextLine( self.axes, 'lap', 'Lap {}'))
+
+    def _addTextLine(self, textLine ):
+        nlines = len(self.textLines)
+        textLine.x = self.x + nlines*self.dx
+        textLine.y = self.y + nlines*self.dy
+        self.textLines.append( textLine )
 
     def ffNames(self):
         """
         Return list of fit file record variable names requred for this plot
         """
-        return [ 'temperature', 'altitude']
+        return [ 'temperature', 'altitude', 'heart_rate']
 
     def update(self, data):
-        for txt_line in self.txt_lines:
+        for txtLine in self.textLines:
 
-            if not txt_line.setValue( data ):
+            if not txtLine.setValue( data ):
                 continue
 
-            txt_line.setAxesText()
+            txtLine.setAxesText()
 
-# Only one bar
-class BarPlot:
-    def __init__(self, ffname, name, units, axes, limit = 100, value = 0.0, scaleFactor=1.0, offSet=0.0 ):
+# Information about a fitfile record to plot
+class PlotVar:
+    def __init__(self, ffname, name, units, maxValue, minValue=0.0, scaleFactor=1.0, offSet=0.0):
         self.ffname = ffname # name in fit file
         self.name = name
         self.units = units
+        self.maxValue = maxValue
+        self.minValue = minValue
         self.scaleFactor = scaleFactor # Multiply ff data by this
         self.offSet = offSet # Add this to ff data
+
+
+    def getNameLabel( self ):
+        return '{} ({})'.format(self.name,self.units)
+
+    def getNormValue( self, data ):
+        """ Between 0 and 1"""
+        return (self.getValue(data) - self.offSet)/(self.maxValue-self.minValue)
+
+    def getValue(self, data ):
+        val = data[self.ffname]
+        return val*self.scaleFactor + self.offSet
+
+    def getValueUnits(self, value ):
+        return '{:.0f} {:}'.format(value,self.units)
+
+class BarPlotBase:
+    alpha = 0.3
+    def __init__(self, plotVars, axes, value = 0.0):
+        self.plotVars = plotVars
         self.axes = axes
         self.axes.autoscale_view('tight')
-        self.axes.set_ylim( 0, limit )
-        self.axes.set_title( self.lable(value),  y=-0.3 )
-        self.bar = self.axes.bar( name, value, alpha=0.5 )
+        self.axes.set_axis_on()
+        self.axes.tick_params(axis=u'both', which=u'both',length=0)
+        for s in ['top','bottom','left','right']:
+            self.axes.spines[s].set_visible(False)
+            
+        self.makeBar( [ pv.name for pv in self.plotVars ] )
+        
+        self.text = []
+        for i in range(len(self.plotVars)):
+            pv = self.plotVars[i]
+            self.appendText(i)
 
     def ffNames(self):
         """
         Return list of fit file variable names requred for this plot
         """
-        return [ self.ffname ]
-
-    def lable(self, value =0.0):
-        return '{:3.0f} {:s}'.format(value,self.units)
+        return [ pv.ffname for pv in self.plotVars ]
     def update(self, data ):
-        value = data[self.ffname]*self.scaleFactor + self.offSet
-        self.bar[0].set_height(value)
-        self.axes.set_title( self.lable(value) )
+        for i in range(len(self.plotVars)) :
+            pv = self.plotVars[i]
+            if not (pv.ffname in data):
+                continue
+
+            value = pv.getValue(data)
+            self.text[i].set_text( pv.getValueUnits(value) )
+
+            # scale the value for the bar chart
+            value = pv.getNormValue(data)
+            self.setBarValue( self.bar[i], value )
+
+    def setBarValue(self, bar, value ):
+        pass
+
+    def appendText(self, i ):
+        pass
+
+    def makeBars(self, names):
+        pass
+
+class BarPlot(BarPlotBase):
+    txt_dx = -0.12
+    txt_dy = 0.05
+    def __init__(self, plotVars, axes, value = 0.0):
+        BarPlotBase.__init__(self, plotVars, axes, value )
+        self.axes.set_ylim( 0.0, 1.0 )
+        self.axes.get_yaxis().set_visible(False)
+
+    def makeBar(self, names ):
+        self.bar = self.axes.bar( x = names, height = [0.0]*len(names), alpha=self.alpha )
+
+
+    def setBarValue(self, bar, value ):
+        bar.set_height( value )
+
+    def appendText(self, i ):
+        pv = self.plotVars[i]
+        self.text.append( self.axes.text( i+self.txt_dx, self.txt_dy, pv.getValueUnits(0.0) ) )
+
+class HBarPlot(BarPlotBase):
+    txt_dx = 0.01
+    txt_dy = -0.15
+    def __init__(self, plotVars, axes, value = 0.0 ):
+        BarPlotBase.__init__(self, plotVars, axes, value )
+        self.axes.set_xlim( 0.0, 1.0 )
+        self.axes.get_xaxis().set_visible(False)
+
+    def makeBar(self, names ):
+        self.bar = self.axes.barh( y = names, width = [0.0]*len(names), alpha=self.alpha )
+
+    def setBarValue(self, bar, value ):
+        bar.set_width( value )
+
+    def appendText(self, i ):
+        pv = self.plotVars[i]
+        self.text.append( self.axes.text( self.txt_dx, i+self.txt_dy, pv.getValueUnits(0.0) ) )
 
 def safeData(d):
     if d is None:
@@ -188,12 +294,8 @@ def prePocessData( infile, timeoffset=None, record_names = ['power','speed','cad
 
 
 def run(data,fig,plots):
-    tstr = datetime.fromtimestamp(int(data['timestamp']))
-    fig.suptitle('{}'.format(tstr))
-
     for plot in plots:
         plot.update(data)
-
 
 # Yeilds to first argument of run()
 class DataGen():
